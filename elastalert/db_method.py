ffee93bab7b5f9180d9ca193c0ac74c2b4d05c1b
yangwm <yangwm@linsdom.com>
2017-06-10 12:50:15 +0800

    db_method

--- elastalert/db_method.py
+++ elastalert/db_method.py
@@ -7,6 +7,7 @@ from db_sqlconn import Mysql
 import config
 import yaml
 import json
+import util_switch
 
 def get_rules_from_db(conf, args):
 
@@ -40,18 +41,45 @@ def get_rules_from_db(conf, args):
                                     "where a.RULE_RECEIVER_TYPE=1 and a.rule_id = %s " \
                                     ") alertpersons "
             query_alertperson_param = [row['id'], row['id']]
-            alertpersons = mysql.getAll(query_alertperson_sql, query_alertperson_param)
+            alertpersons = mysql.getAll(query_alertperson_sql, query_alertperson_param) #数据库查询的接收者
+            emails=[]
+            for alertemail in alertpersons:
+                emails.append(alertemail['user_email'])
+                #print "数组=========",emails
 
-            if row['rule_type'] == 'frequency':
+            """if row['rule_type'] == 'frequency':
+                rule = frequency_rule(conf, row) #frequency规则模板填充数据"""
 
-                rule = frequency_rule(conf, row)
+            row['user_email']= emails
+            for case in util_switch.switch(row['rule_type']):
+                if case('frequency'):
+                    rule = frequency_rule(conf, row)
+                    config.load_options(rule, conf, '', args)
+                    config.load_modules(rule, args)
+                    rule['alertpersons'] = alertpersons
+                    rule['rule_id'] = row['id']
+                    rule['rule_type'] = row['rule_type']
+                    rules.append(rule)
+                    break
+                if case('spike'):
+                    rule = spike_rule(conf, row)
+                    config.load_options(rule, conf, '', args)
+                    config.load_modules(rule, args)
+                    rule['alertpersons'] = alertpersons
+                    rule['rule_id'] = row['id']
+                    rule['rule_type'] = row['rule_type']
+                    rules.append(rule)
+                    break
 
-            config.load_options(rule, conf, '', args)
+                if case():  # 默认
+                    print "switch something else!"
+
+        """  config.load_options(rule, conf, '', args)
             config.load_modules(rule, args)
             rule['alertpersons'] = alertpersons
             rule['rule_id'] = row['id']
-            rule['rule_type'] = 'frequency'
-            rules.append(rule)
+            rule['rule_type'] =row['rule_type']
+            rules.append(rule)"""
 
     # 释放资源
     mysql.dispose()
@@ -69,15 +97,15 @@ def frequency_rule(conf, row):
             'smtp_port': conf['smtp_port'],
             'smtp_auth_file': '',
             'num_events': 1,
-            'alert': ['db'],
+            'alert': ['db','email'],
             'filter': [{'query': {'query_string': {'query': 'field6:Exception'}}}],
             'email_reply_to': '',
-            'rule_file': '/Users/yangwm/log/elastalert/example_rules/example_frequency.yaml',
+            'rule_file': '',
             'timeframe': {'minutes': 1},
             'type': 'frequency',
-            'email': ['286388651@qq.com']}
+            'email': ['1391464709@qq.com']}
 
-    #rule_filter_str = row["rule_filter"].replace("\n", "").strip()
+    #rule_filter_str = row["rule_filter"].replace("\n", "").strip()/Users/yangwm/log/elastalert/example_rules/example_frequency.yaml
     rule_extend_str = row["rule_extend"].replace("\n", "").strip()
     #rule_filter = json.loads(rule_filter_str)
     rule_extend = json.loads(rule_extend_str)
@@ -91,8 +119,8 @@ def frequency_rule(conf, row):
     rule['timeframe'] = rule_extend['timeframe']
     rule['type'] = 'frequency'
     rule['filter'] = rule_extend['filter']
-
-    print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
+    rule['email'] = row['user_email']
+    print "frequency=========%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
         row["id"], row["rule_name"], row["rule_type"], row["rule_index"], row["rule_desc"], row["rule_alerts"],
         row["rule_filter"], row["rule_extend"], row["rule_seq"])
 
@@ -109,13 +137,35 @@ def spike_rule(conf, row):
             'smtp_port': conf['smtp_port'],
             'smtp_auth_file': '',
             'num_events': 1,
-            'alert': ['db'],
-            'filter': [{'query': {'query_string': {'query': 'field6:Exception'}}}],
+            'alert': ['db','email'],
+            'filter': [{'query': {'query_string': {'query': '_id:AVwGceqjqjtyRJ_Calx5'}},'type':{'value':'spike'}}],
             'email_reply_to': '',
-            'rule_file': 'example_rules/example_frequency.yaml',
-            'timeframe': {'minutes': 1},
-            'type': 'frequency',
-            'email': ['286388651@qq.com']}
+            'rule_file': '',
+            'timeframe': {'minutes':1},
+            'type': 'spike',
+            'spike_height': 1,
+            'threshold_cur':1,
+            'spike_type': 'up',
+            'email': ['1391464709@qq.com']}
+
+    rule_extend_str = row["rule_extend"].replace("\n", "").strip()
+    rule_extend = json.loads(rule_extend_str)
+    rule['filter'] = rule_extend['filter']
+    rule['name'] = row["rule_name"]
+    rule['index'] = row["rule_index"]
+    alert_way = row['rule_alerts']
+    rule['alert'] = alert_way.split(',')
+    rule['alert_way'] = row['rule_alerts']
+    rule['email']=row['user_email']
+    rule['spike_height'] = rule_extend['spike_height']
+    rule['threshold_cur'] = rule_extend['threshold_cur']
+    rule['spike_type'] = rule_extend['spike_type']
+    rule['timeframe'] = rule_extend['timeframe']
+
+
+    print "spike=============%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
+        row["id"], row["rule_name"], row["rule_type"], row["rule_index"], row["rule_desc"], row["rule_alerts"],
+        row["rule_filter"], row["rule_extend"], row["rule_seq"])
 
     return rule
 
